f186f55f98e8972e16dbf405d660c0e2a9c1fbe7
yordi68 <yordanos.yirgu1@gmail.com>
2024-03-15 19:34:44 +0300

    event details page integration with backend

--- components/Ui/VerticalCard.vue
+++ components/Ui/VerticalCard.vue
@@ -6,6 +6,8 @@ import { toast } from "vue3-toastify";
 let isOwner = false;
 const isFollowed = true;
 const store = useAuthStore();
+
+
 const props = defineProps({
         event: {
                 type: Object,
@@ -71,7 +73,7 @@ const formattedDateTime = computed(() => {
 });
 
 
-console.log(props.event)
+// console.log(props.event)
 
 
 </script>
@@ -88,58 +90,61 @@ console.log(props.event)
 
 <template>
         <div
-                class="w-full h-96 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <img class=" w-full h-1/2 rounded-t-lg object-fit" :src="event.thumbnail" alt="" />
-                <div class="p-5 flex flex-col space-y-2">
+                class="w-full h-96 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:cursor-pointer">
+                <div @click="$router.push(`/events/${event.id}`)">
 
-                        <h5 class="text-wrap text-bold  text-md md:text-xl">
-                                {{ event.title }}
-                        </h5>
+                        <img class=" w-full h-1/2 rounded-t-lg object-fit" :src="event.thumbnail" alt="" />
+                        <div class="p-5 flex flex-col space-y-2">
 
-                        <div class="flex space-x-3 text-sm  md:text-md">
-                                <p>{{ formattedDateTime.formattedDate }}</p>
-                                <p>{{ formattedDateTime.formattedTime }}</p>
-                                <div class="h-full bg-black w-px"></div>
-                                <p>{{ event.place }}</p>
+                                <h5 class="text-wrap text-bold  text-md md:text-xl">
+                                        {{ event.title }}
+                                </h5>
+
+                                <div class="flex space-x-3 text-sm  md:text-md">
+                                        <p>{{ formattedDateTime.formattedDate }}</p>
+                                        <p>{{ formattedDateTime.formattedTime }}</p>
+                                        <div class="h-full bg-black w-px"></div>
+                                        <p>{{ event.place }}</p>
+                                </div>
+                                <p>{{ event.date }}</p>
+                                <span class="flex flex-col items-start  space-x-4 ">
+                                        <div class="flex">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                                                        fill="#008000" class="w-6 h-6">
+                                                        <path
+                                                                d="M10.464 8.746c.227-.18.497-.311.786-.394v2.795a2.252 2.252 0 0 1-.786-.393c-.394-.313-.546-.681-.546-1.004 0-.323.152-.691.546-1.004ZM12.75 15.662v-2.824c.347.085.664.228.921.421.427.32.579.686.579.991 0 .305-.152.671-.579.991a2.534 2.534 0 0 1-.921.42Z" />
+                                                        <path fill-rule="evenodd"
+                                                                d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v.816a3.836 3.836 0 0 0-1.72.756c-.712.566-1.112 1.35-1.112 2.178 0 .829.4 1.612 1.113 2.178.502.4 1.102.647 1.719.756v2.978a2.536 2.536 0 0 1-.921-.421l-.879-.66a.75.75 0 0 0-.9 1.2l.879.66c.533.4 1.169.645 1.821.75V18a.75.75 0 0 0 1.5 0v-.81a4.124 4.124 0 0 0 1.821-.749c.745-.559 1.179-1.344 1.179-2.191 0-.847-.434-1.632-1.179-2.191a4.122 4.122 0 0 0-1.821-.75V8.354c.29.082.559.213.786.393l.415.33a.75.75 0 0 0 .933-1.175l-.415-.33a3.836 3.836 0 0 0-1.719-.755V6Z"
+                                                                clip-rule="evenodd" />
+                                                </svg>
+
+                                                {{ event.price }}
+                                        </div>
+                                        {{ event.user.first_name }}
+                                        {{ event.user.last_name }}
+
+                                </span>
+
+
+                                <div class="flex items-start justify-start space-x-8" v-if="isOwner">
+                                        <div @click="$router.push(`/user/event/edit/${event.id}`)"
+                                                class="text-xl cursor-pointer">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                        stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                                </svg>
+                                        </div>
+                                        <div>
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                        stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                                </svg>
+                                        </div>
+                                </div>
+
                         </div>
-                        <p>{{ event.date }}</p>
-                        <span class="flex flex-col items-start  space-x-4 ">
-                                <div class="flex">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#008000"
-                                                class="w-6 h-6">
-                                                <path
-                                                        d="M10.464 8.746c.227-.18.497-.311.786-.394v2.795a2.252 2.252 0 0 1-.786-.393c-.394-.313-.546-.681-.546-1.004 0-.323.152-.691.546-1.004ZM12.75 15.662v-2.824c.347.085.664.228.921.421.427.32.579.686.579.991 0 .305-.152.671-.579.991a2.534 2.534 0 0 1-.921.42Z" />
-                                                <path fill-rule="evenodd"
-                                                        d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v.816a3.836 3.836 0 0 0-1.72.756c-.712.566-1.112 1.35-1.112 2.178 0 .829.4 1.612 1.113 2.178.502.4 1.102.647 1.719.756v2.978a2.536 2.536 0 0 1-.921-.421l-.879-.66a.75.75 0 0 0-.9 1.2l.879.66c.533.4 1.169.645 1.821.75V18a.75.75 0 0 0 1.5 0v-.81a4.124 4.124 0 0 0 1.821-.749c.745-.559 1.179-1.344 1.179-2.191 0-.847-.434-1.632-1.179-2.191a4.122 4.122 0 0 0-1.821-.75V8.354c.29.082.559.213.786.393l.415.33a.75.75 0 0 0 .933-1.175l-.415-.33a3.836 3.836 0 0 0-1.719-.755V6Z"
-                                                        clip-rule="evenodd" />
-                                        </svg>
-
-                                        {{ event.price }}
-                                </div>
-                                {{ event.user.first_name }}
-                                {{ event.user.last_name }}
-
-                        </span>
-
-
-                        <div class="flex items-start justify-start space-x-8" v-if="isOwner">
-                                <div @click="$router.push(`/user/event/edit/${event.id}`)"
-                                        class="text-xl cursor-pointer">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                                        </svg>
-                                </div>
-                                <div>
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                        </svg>
-                                </div>
-                        </div>
-
                 </div>
         </div>
 </template>
---
+++ pages/events/[id].vue
@@ -0,0 +1,250 @@
+<script setup>
+import getEvent from "~/graphql/query/events/item.gql";
+import { useAuthStore } from "~/stores/auth";
+import { toast } from "vue3-toastify";
+
+
+const store = useAuthStore();
+const route = useRoute();
+
+const event = ref({});
+
+const { onResult, onError, refetch } = singleQuery(getEvent, {
+    id: route.params.id,
+});
+
+onResult((result) => {
+    event.value = { ...result.data.events_by_pk };
+    console.log("Hi this is me", event.value);
+})
+
+onError((error) => {
+    console.log(error, "error");
+})
+
+
+
+
+
+const datetime = ref(event.value.time);
+
+const formattedDateTime = computed(() => {
+    const dateOptions = { weekday: 'short', month: 'short', day: '2-digit', year: 'numeric' };
+    const timeOptions = { hour: 'numeric', minute: 'numeric', hour12: true };
+
+    const parsedDatetime = new Date(datetime.value);
+
+    const formattedDate = parsedDatetime.toLocaleString('en-US', dateOptions).replace(',', '/');
+    const formattedTime = parsedDatetime.toLocaleString('en-US', timeOptions);
+
+    return { formattedDate, formattedTime }
+});
+
+
+
+
+
+
+
+
+
+const eventLocation = ref(
+    {
+        "id": 12,
+        "location": {
+            "type": "Point",
+            "crs": {
+                "type": "name",
+                "properties": {
+                    "name": "urn:ogc:def:crs:EPSG::4326"
+                }
+            },
+            "coordinates": [
+                9.014253,
+                38.818221
+            ]
+        },
+        "area": {
+            "name": "Megenagna",
+            "__typename": "BasicsAreas"
+        },
+        "city": {
+            "name": "Addis Ababa",
+            "__typename": "BasicsCities"
+        },
+    },
+
+)
+
+
+
+</script>
+
+
+
+
+
+
+
+
+
+
+<template>
+    <div class="px-32">
+        <div class="w-full mt-5">
+            <img :src="event.thumbnail" alt="random image" class="w-full h-full object-cover " />
+        </div>
+        <div class="flex justify-between my-6">
+            <h3 class="font-bold text-4xl">{{ event.title }}</h3>
+            <div>
+                <Icon name="material-symbols-light:star-outline" size="48" />
+                <Icon name="ic:baseline-share" size="32" />
+            </div>
+        </div>
+
+        <div class="flex justify-between">
+            <div class="space-y-4">
+                <h3 class="text-xl font-semibold">Date and Time</h3>
+                <div class="flex space-x-2">
+
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+                    </svg>
+                    <p>{{ new Date(event.time).toDateString() }}</p>
+                </div>
+                <div class="flex space-x-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                    <p>{{ new Date(event.time).toLocaleTimeString() }}</p>
+
+                </div>
+
+            </div>
+            <div class="space-y-6">
+                <button class="flex bg-[#FFE047] rounded-md items-center py-4 px-6 space-x-4 ">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 0 1 0 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 0 1 0-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375Z" />
+                    </svg>
+                    <p class="font-bold text-lg">
+                        Buy Tickets
+                    </p>
+                </button>
+                <div class="space-y-4">
+                    <h3 class="font-bold text-lg">Ticket Information</h3>
+                    <div class="flex space-x-2">
+
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 0 1 0 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 0 1 0-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375Z" />
+                        </svg>
+                        <p class="text-sm">
+                            Standard Ticket: {{ event.price }} each
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="w-full flex justify-between">
+
+            <div class="w-1/3 ">
+                <h3 class="text-3xl font-bold">Location</h3>
+                <div class="flex items-center justify-start space-x-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-24 h-24">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                    </svg>
+
+                    <p class="text-wrap">
+                        Bal Gandharva Rang Mandir, Near Junction Of 24th & 32nd Road &
+                        Patwardhan
+                        Park,Off Linking Road, Bandra West., Mumbai, India
+                    </p>
+
+                </div>
+                <div>
+                    <EventsLocation :event-location="eventLocation"></EventsLocation>
+                </div>
+            </div>
+
+            <div class="w-1/3 my-8  flex flex-col items-end">
+                <h3 class="text-3xl font-bold my-4 pr-32">Hosted by</h3>
+                <div class="w-2/3">
+                    <div class="flex items-center justify-evenly">
+                        <div>
+                            <img class="w-24 h-24 mb-3 rounded-full shadow-lg"
+                                src="https://source.unsplash.com/100x100/?portrait" alt="Bonnie image" />
+                        </div>
+                        <div>
+
+                            <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+                                {{ event.user.first_name }} {{ event.user.last_name }}
+                            </h5>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">Visual
+                                Designer</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+        <!-- <div class="w-1/3 my-8">
+                        <h3 class="text-3xl font-bold my-4">Hosted by</h3>
+                        <div class="w-2/3">
+                                <div class="flex items-center justify-evenly">
+                                        <div>
+                                                <img class="w-24 h-24 mb-3 rounded-full shadow-lg"
+                                                        src="https://source.unsplash.com/100x100/?portrait"
+                                                        alt="Bonnie image" />
+                                        </div>
+                                        <div>
+
+                                                <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+                                                        Bonnie Green
+                                                </h5>
+                                                <span class="text-sm text-gray-500 dark:text-gray-400">Visual
+                                                        Designer</span>
+                                        </div>
+                                </div>
+                        </div>
+                </div> -->
+
+
+        <div class="w-full my-8">
+            <h3 class="text-3xl font-bold my-4">Event Description</h3>
+            <p>
+                {{ event.description }}
+            </p>
+        </div>
+
+
+
+
+
+
+
+
+        <div class="w-1/2 my-8 ">
+            <h3 class="text-3xl font-bold my-4">Tags</h3>
+            <div class="flex items-center justify-start flex-wrap">
+                <UiTag v-for="tag in event.tags" :key="tag.id" :tagName="tag.tag.name" />
+            </div>
+
+        </div>
+    </div>
+
+</template>
