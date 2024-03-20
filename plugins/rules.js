4f60ffcba6788046c4a5cd23fbad78c488942d8d
yordi68 <yordanos.yirgu1@gmail.com>
2024-03-20 10:04:09 +0300

    fix plugins

--- plugins/rules.js
+++ plugins/rules.js
@@ -1,31 +1,12 @@
 import { defineRule } from "vee-validate";
 
 export default defineNuxtPlugin((vueApp) => {
-  defineRule("onerequired", (value, [], ctx) => {
-    return !!value || value?.length || "At least one selection required";
+  defineRule("required", (value, []) => {
+    return !!value || value?.length || "Please fill this field";
   }),
-    defineRule("required", (value, [], ctx) => {
-      return !!value || value?.length || "Please fill this field";
-    }),
-    defineRule("boolReq", (value, [], ctx) => {
-      return typeof value == "boolean" || "Field Required";
-    });
-
-  defineRule("array_object_required", (value, [], ctx) => {
-    return value?.length || "Field Required";
-  }),
-    defineRule("yes_or_no", (value, [], ctx) => {
-      return value?.length || "Field Required";
-    }),
     defineRule("number", (value) => {
       return !value || /^[0-9]+$/.test(value) || "Number only";
     }),
-    defineRule("numberFromZero", (value, [], ctx) => {
-      return /^(?:0|[1-9]\d*)$/.test(value) || "Number only";
-    });
-  defineRule("numrange", (value) => {
-    return !value || /^[0-9-]+$/.test(value) || "Number only";
-  }),
     defineRule("email", (value) => {
       return (
         !value ||
@@ -42,17 +23,13 @@ export default defineNuxtPlugin((vueApp) => {
         return `${context.field} is must be greater than ${context.rule.params[0]}`;
       }
     });
-
-  defineRule("ethio_phone", (value) => {
-    return !value || /^(7|9)\d{8}$/.test(value) || "Not valid phone number";
+  defineRule("password", (value) => {
+    return (
+      !value ||
+      value.length >= 6 ||
+      "Password Must be greater than 6 characters"
+    );
   }),
-    defineRule("password", (value) => {
-      return (
-        !value ||
-        value.length >= 6 ||
-        "Password Must be greater than 6 characters"
-      );
-    }),
     defineRule("confirmed", (value, [other]) => {
       return (
         !value ||
