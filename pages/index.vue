5a9d6bde54a217c83ba93b2114acf89c82271d78
yordi68 <yordanos.yirgu1@gmail.com>
2024-03-21 09:41:20 +0300

    add loading cards to home page

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@ const filter = computed(() => {
 });
 
 /*--------------------- Fetching Latest Events ----------------------- */
-const { onResult: latestEventResult, onError: latestEventError, refetch: latestEventRefetch } = queryList(getEvents, {
+const { onResult: latestEventResult, onError: latestEventError, refetch: latestEventRefetch, loading: latestEventLoading } = queryList(getEvents, {
         filter: filter,
         order: ref({ "created_at": "desc" }),
         offset: 0,
@@ -46,7 +46,7 @@ latestEventError((error) => {
 
 
 /*--------------------- Fetching Popular Events ----------------------- */
-const { onResult: popularEventResult, onError: popularEventError, refetch: popularEventRefetch } = queryList(getEvents, {
+const { onResult: popularEventResult, onError: popularEventError, refetch: popularEventRefetch, loading: popularEventLoading } = queryList(getEvents, {
         filter: filter,
         order: ref({
                 "follows_aggregate": {
@@ -100,7 +100,7 @@ definePageMeta({
 
         <div>
                 <UiHero />
-                <div class="px-8 md:px-24">
+                <!-- <div class="px-8 md:px-24">
                         <h3 class="text-3xl font-bold my-4 pr-32">Popular Events</h3>
                         <div class="grid grid-cols-1 gap-y-8 md:grid-cols-3  md:gap-x-4 ">
                                 <UiVerticalCard v-for="popularEvent in popularEvents" :key="popularEvent.price"
@@ -115,6 +115,72 @@ definePageMeta({
                                 <UiVerticalCard v-for="latestEvent in latestEvents" :key="latestEvent.price"
                                         :event="latestEvent" @refetch="latestEventRefetch" />
                         </div>
+                </div> -->
+                <div class="px-8 md:px-24 md:py-16">
+                        <h3 class="text-3xl  font-bold my-4 pr-32">Popular Events</h3>
+                        <hr class="my-6 border-gray-200 w-3/4 ">
+                        <div class="pt-4 overflow-y-scroll col-span-12 lg:col-span-10 h-full"
+                                v-if="!popularEventLoading">
+                                <div v-if="latestEvents && latestEvents.length > 0">
+                                        <div
+                                                class="grid grid-cols-1 md:grid-cols-3 md:gap-4 mx-auto py-6 sm:px-6 lg:px-8">
+                                                <UiVerticalCard @refetch="popularEventRefetch"
+                                                        v-for="popularEvent in popularEvents" :key="popularEvent.price"
+                                                        :event="popularEvent" />
+                                        </div>
+                                </div>
+                                <div v-else class="flex flex-col items-center justify-center my-auto ">
+
+                                        <img src="/noEvents-2.svg" class="max-w-32" />
+                                        <span class="text-md font-semibold">Sorry! We couldn't find any events</span>
+                                </div>
+                        </div>
+
+                        <div class="col-span-10 " v-else>
+                                <div
+                                        class="overflow-y-scroll col-span-12 lg:col-span-10 h-full  border-l-2 border-gray-200">
+                                        <div
+                                                class="grid grid-cols-1 md:grid-cols-3 md:gap-4 mx-auto py-6 sm:px-6 lg:px-8">
+                                                <UiLoadingCard v-for="index in 9" :key="index" />
+                                        </div>
+
+                                </div>
+                        </div>
+                </div>
+
+                <div class="px-8 md:px-24">
+                        <h3 class="text-3xl  font-bold my-4 pr-32">Latest Events</h3>
+                        <hr class="my-6 border-gray-200 w-3/4 ">
+                        <div class="pt-4 overflow-y-scroll col-span-12 lg:col-span-10 h-full"
+                                v-if="!latestEventLoading">
+                                <div v-if="latestEvents && latestEvents.length > 0">
+                                        <div
+                                                class="grid grid-cols-1 md:grid-cols-3 md:gap-4 mx-auto py-6 sm:px-6 lg:px-8">
+                                                <UiVerticalCard @refetch="latestEventRefetch" v-for="
+                                                latestEvent in latestEvents" :key="latestEvent.price"
+                                                        :event="latestEvent" />
+                                        </div>
+                                </div>
+                                <div v-else class="flex flex-col items-center justify-center my-auto ">
+
+                                        <img src="/noEvents-2.svg" class="max-w-32" />
+                                        <span class="text-md font-semibold">Sorry! We couldn't find any events</span>
+                                </div>
+                        </div>
+
+
+
+                        <div class="col-span-10 " v-else>
+                                <div
+                                        class="overflow-y-scroll col-span-12 lg:col-span-10 h-full  border-l-2 border-gray-200">
+                                        <div
+                                                class="grid grid-cols-1 md:grid-cols-3 md:gap-4 mx-auto py-6 sm:px-6 lg:px-8">
+                                                <UiLoadingCard v-for="index in 9" :key="index" />
+                                        </div>
+
+                                </div>
+                        </div>
+
                 </div>
 
 
--- pages/user/my-events.vue
+++ pages/user/my-events.vue
@@ -26,7 +26,7 @@ const filter = computed(() => {
 // console.log(filter.value)
 
 /*------------------------- Query the users event ---------------------- */
-const { onResult, onError, refetch } = queryList(
+const { onResult, onError, refetch, loading } = queryList(
         getEvents, {
         filter: filter,
         clientId: ref('auth')
@@ -48,8 +48,27 @@ definePageMeta({
 })
 </script>
 <template>
-        <div class="grid grid-cols-3 gap-6">
-                <!-- <UiCard has-icon v-for="event in events" :key="event.price" :event="event" /> -->
-                <UiVerticalCard v-for="event in events" :key="event.price" :event="event" @refetch="refetch" />
+        <div class="pt-4 overflow-y-scroll col-span-12 lg:col-span-10 h-full  border-l-2 border-gray-200"
+                v-if="!loading">
+                <div v-if="events && events.length > 0">
+                        <div class="grid grid-cols-1 md:grid-cols-3 md:gap-4 mx-auto py-6 sm:px-6 lg:px-8">
+                                <UiVerticalCard @refetch="refetch" v-for="event in events" :key="event.price"
+                                        :event="event" />
+                        </div>
+                </div>
+                <div v-else class="flex flex-col items-center justify-center my-auto ">
+
+                        <img src="/noEvents-2.svg" class="max-w-32" />
+                        <span class="text-md font-semibold">Sorry! We couldn't find any events</span>
+                </div>
+        </div>
+
+        <div class="col-span-10 " v-else>
+                <div class="overflow-y-scroll col-span-12 lg:col-span-10 h-full  border-l-2 border-gray-200">
+                        <div class="grid grid-cols-1 md:grid-cols-3 md:gap-4 mx-auto py-6 sm:px-6 lg:px-8">
+                                <UiLoadingCard v-for="index in 9" :key="index" />
+                        </div>
+
+                </div>
         </div>
 </template>
